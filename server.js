32e1e6b7854e62103d46466a2a3ea55509f00bda
yiling <yiling@25sprout.com>
2024-04-28 12:35:58 +0800

    add mongoose and connect to DB

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const http = require('http');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const headers = require('./headers')
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB)
+	.then(() => {
+		console.log('DB connection successful');
+	})
+	.catch((err) => {
+		console.log('DB connection failed', err);
+	})
 
 const requestListener = (req, res) => {
 	if (req.url === '/') {
