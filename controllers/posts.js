c02d03b6b3662a8abd03ea969a2fcc842e7c5018
yiling <yiling@25sprout.com>
2024-05-02 21:38:39 +0800

    modularizing posts controllers

---
+++ controllers/posts.js
@@ -0,0 +1,47 @@
+const errorHandler = require('../service/errorHandler');
+const successHandler = require('../service/successHandler');
+const Post = require('../models/posts');
+
+const posts = {
+	async getPosts(res) {
+		const data = await Post.find();
+		successHandler(res, '取得所有 posts 成功', data);
+		res.end();
+	},
+	async createPosts({req, res, body}) {
+		try {
+			const data = JSON.parse(body);
+			const newPost = await Post.create(data);
+			successHandler(res, '新增 post 成功', newPost);
+		} catch (err) {
+			errorHandler(res, err);
+		}
+	},
+	async deleteAllPosts(res) {
+		try {
+			const deletePosts = await Post.deleteMany();
+			successHandler(res, '刪除所有 posts 成功', deletePosts);
+		} catch (err) {
+			errorHandler(res, err);
+		}
+	},
+	async deletePost({res, postId}) {
+		try {
+			const deletePost = await Post.findByIdAndDelete(postId);
+			successHandler(res, '刪除 post 成功', deletePost);
+		} catch (err) {
+			errorHandler(res, err);
+		}
+	},
+	async updatePost({res, postId, body}) {
+		try {
+			const data = JSON.parse(body);
+			const updatePost = await Post.findByIdAndUpdate(postId, data, { runValidators: true, new: true });
+			successHandler(res, '更新 post 成功', updatePost);
+		} catch (err) {
+			errorHandler(res, err);
+		}
+	}
+};
+
+module.exports = posts;
--- routes/index.js
+++ routes/index.js
@@ -1,67 +1,36 @@
-const headers = require('../service/headers');
+const HttpController = require('../controllers/http');
+const postsController = require('../controllers/posts');
 const errorHandler = require('../service/errorHandler');
 const successHandler = require('../service/successHandler');
 const Post = require('../models/posts');
+const posts = require('../controllers/posts');
 
 const routes = async(req, res) => {
-	const urlParams = req.url.split('/');
+	const { url, method } = req;
+	const urlParams = url.split('/');
 	let body = '';
 	req.on('data', chunk => {
 		body += chunk;
 	})
-	if (req.url === '/posts' && req.method === 'GET') {
-		try {
-			const data = await Post.find();
-			successHandler(res, '取得所有 posts 成功', data);
-		} catch (err) {
-			errorHandler(res, err);
-		}
-	} else if (req.url === '/posts' && req.method === 'POST') {
-		req.on('end', async () => {
-			try {
-				const data = JSON.parse(body);
-				const newPost = await Post.create(data);
-				successHandler(res, '新增 post 成功', newPost);
-			} catch (err) {
-				errorHandler(res, err);
-			}
-		})
-
-	} else if (req.url === '/posts' && req.method === 'DELETE') {
-		try {
-			const deletePosts = await Post.deleteMany();
-			successHandler(res, '刪除所有 posts 成功', deletePosts);
-		} catch (err) {
-			errorHandler(res, err);
-		}
-	} else if (req.url.includes('/posts/') && req.method === 'DELETE') {
+	if (url === '/posts' && method === 'GET') {
+		postsController.getPosts(res);
+	} else if (url === '/posts' && method === 'POST') {
+		req.on('end', () =>  postsController.createPosts({body, req, res}))
+	} else if (url === '/posts' && method === 'DELETE') {
+		postsController.deleteAllPosts(res);
+	} else if (url.includes('/posts/') && method === 'DELETE') {
 		const postId = urlParams[2];
-		try {
-			const deletePost = await Post.findByIdAndDelete(postId);
-			successHandler(res, '刪除 post 成功', deletePost);
-		} catch (err) {
-			errorHandler(res, err);
-		}
-	} else if (req.url.includes('/posts/') && req.method === 'PATCH') {
+		postsController.deletePost({res, postId});
+	} else if (url.includes('/posts/') && method === 'PATCH') {
 		const postId = urlParams[2];
-
 		req.on('end', async () => {
-			try {
-				const data = JSON.parse(body);
-				const updatePost = await Post.findByIdAndUpdate(postId, data, { runValidators: true, new: true });
-				successHandler(res, '更新 post 成功', updatePost);
-			} catch (err) {
-				errorHandler(res, err);
-			}
+			postsController.updatePost({res, postId, body});
 		});
-	} else if (req.method === 'OPTIONS') {
-		res.writeHead(200, headers);
-		res.end();
+	} else if (method === 'OPTIONS') {
+		HttpController.cors(req, res);
 	}
 	else {
-		res.writeHead(404, headers);
-		res.write("Page not found");
-		res.end();
+		HttpController.notFound(req, res);
 	}
 };
 
